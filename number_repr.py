737faa5ffc7d39a882b78481ed60d369e3ce5a22
Jesse Maggard <jesse.maggard@gmail.com>
2020-04-16 13:33:32 -0600

    Add files via upload

--- number_repr.py
+++ number_repr.py
@@ -1,12 +1,16 @@
-x = 10_000_000
-y = 1_000_000_000_000
+num1 = 10_000_000
+num2 = 1_000_000_000_000
 
 total = num1 + num2
 
 print(total)
 
+#returns hard to read number  1000100000001000000 
+
 
 #hard to read the numbers, add commas by underscoring and changing the underscores later
 # underscore does not change the math, it is replaced in f string formatting via the colon :
 
-print(f'{total:r}')
+print(f'{total:,}')
+
+#returns 1,000,000,000,000,000,000
